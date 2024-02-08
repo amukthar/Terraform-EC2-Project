6a7b7e0f5cc15fde12d62d5ec72babe08890ed3a
Amuktha Rao <jammula.d@gmail.com>
2024-02-08 13:00:03 -0500

    Added subnet in variables.tf and main.tf

--- main.tf
+++ main.tf
@@ -13,3 +13,24 @@ resource "aws_vpc" "vpc_test" {
       Name = "vpc_test"
   }
 }
+
+resource "aws_subnet" "public_subnet_test" {
+  count                  = length(var.public_subnet_cidr_blocks)
+  vpc_id                 = "${aws_vpc.vpc_test.id}"
+  cidr_block             = var.public_subnet_cidr_blocks[count.index]
+  availability_zone      = element(var.aws_availability_zones, count.index)
+  map_public_ip_on_launch = true
+  tags = {
+      Name = "public_subnet_test"
+  }
+}
+
+resource "aws_subnet" "private_subnet_test" {
+  count                  = length(var.private_subnet_cidr_blocks)
+  vpc_id                 = aws_vpc.vpc_test.id
+  cidr_block             = var.private_subnet_cidr_blocks[count.index]
+  availability_zone      = element(var.aws_availability_zones, count.index)
+  tags = {
+      Name = "private_subnet_test"
+  }
+}
--- variables.tf
+++ variables.tf
@@ -2,3 +2,21 @@ variable "vpc_cidr_block" {
   description = "CIDR block for the VPC"
   default     = "10.0.0.0/16"
 }
+
+variable "public_subnet_cidr_blocks" {
+  description = "CIDR blocks for public subnets"
+  type        = list(string)
+  default     = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
+}
+
+variable "private_subnet_cidr_blocks" {
+  description = "CIDR blocks for private subnets"
+  type        = list(string)
+  default     = ["10.0.4.0/24", "10.0.5.0/24", "10.0.6.0/24"]
+}
+
+variable "aws_availability_zones" {
+  description = "Availability zones for subnets"
+  type        = list(string)
+  default     = ["us-east-1a", "us-east-1b", "us-east-1c"]
+}
